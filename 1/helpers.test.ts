5e522662a95c408c1851be297432b508cae3a3da
Sophia Zavala <sophie0z.sz@gmail.com>
2021-12-02 00:37:25 +0000

    Finish getData function and add test

--- 1/helpers.test.ts
+++ 1/helpers.test.ts
@@ -1,5 +1,9 @@
 import { getData } from "./helpers"
 
-describe('getData', ()=> {
-    
+describe('getData', () => {
+    test('it should return formatted data', async () => {
+        const data = await getData('./1/testdata.txt')
+        expect(data.length).toEqual(10)
+        expect(data[0]).toEqual(199)
+    })
 })
--- 1/helpers.ts
+++ 1/helpers.ts
@@ -1,9 +1,13 @@
-import {readFile} from 'fs/promises'
+import * as fs from "fs"
+import { PathLike } from 'graceful-fs'
 
 //this is a promise
 //this is an arrow function in javascript
-export const getData = async (fileName) => {
-    const path = fileName || './1/testdata.txt'
-    const rawData = await readFile(path)
+export const getData = async (fileName?:PathLike): Promise<number[]> => {
+    const path:PathLike = fileName || './1/data.txt'
+    const rawData = await fs.promises.readFile(path, {encoding:'utf-8'})
+    const formattedData = rawData.split('\n').filter(x => x).map(x => parseInt(x))
+
+    return formattedData
 }
 
