2c0f5ae765b75ff454208efa2ff0b0b1a0510b9b
Sophia Zavala <sophie0z.sz@gmail.com>
2021-12-03 00:06:14 +0000

    Fixing typescript errors

--- 1/helpers.ts
+++ 1/helpers.ts
@@ -1,13 +1,13 @@
 //this is a promise
 import * as fs from 'fs'
-
+import { PathLike } from 'graceful-fs'
 
 //this is an arrow function in javascript
 //export is there to be able to use it from anywhere else in our code and we will test it by importing it 
 //into our test file
 //an asyncornys function will return a promise so we will have to wait for that promise to finish 
 // so we have to make sure the promise has finished
-export const getData = async (fileName) => {
+export const getData = async (fileName?:PathLike):Promise<number[]> => {
     //going in to testdata to get the data so we need a path
     const path = fileName || './1/testdata.txt'
     //this one is for waiting for the promise to finish
@@ -25,7 +25,7 @@ export const getData = async (fileName) => {
 
 }
 //making our function
-export const countIncreasedMeasurements = (data) => {
+export const countIncreasedMeasurements = (data:number[]):number => {
 //counting 
     let count = 0
     //for loop counting the data increases
--- 1/index.ts
+++ 1/index.ts
@@ -2,7 +2,7 @@
 import { getData, countIncreasedMeasurements } from "./helpers"
 //because we have the await, the main function is now async
 const main = async() => {
-    const data = await getData("")
+    const data = await getData()
     const answerIncrease = countIncreasedMeasurements(data)
     
     
